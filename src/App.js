69a5f94905197fa6a0471a3b1aae602f39b5c1d4
sherzod38 <72493786+sherzod38@users.noreply.github.com>
2021-05-02 15:55:00 +0500

    f1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
+import {
+  HomePage,
+  SingleCityPage
+} from './pages';
+
+import Header from './containers/Header';
+
+import './assets/main.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header/>
+
+        <Switch>
+          <Route exact path="/" component={HomePage}/>
+          <Route exact path="/city/:cityName" component={SingleCityPage}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
